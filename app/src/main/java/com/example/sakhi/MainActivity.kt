4fda2f28bc79e2781548706e6b20197433665ec6
samarpitabdas <samarpitabdas@gmail.com>
2024-11-27 21:23:10 +0530

    bottom navigation bar

---
+++ app/src/main/java/com/example/sakhi/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.sakhi
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.sakhi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        replacefragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replacefragment(HomeFragment())
+                R.id.emergency-> replacefragment(Emergency())
+                R.id.cases -> replacefragment(Cases())
+                R.id.settings -> replacefragment(Settings())
+
+                else ->{
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replacefragment(fragment:Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
+}
